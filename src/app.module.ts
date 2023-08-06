88aae541b2b84ed9860d6ff159c441251e8c601c
Khoo Hao Yit <khoohaoyit16@gmail.com>
2023-08-06 08:44:36 +0000

    add shared secreth auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'nestjs-prisma';
+import { AuthModule } from './auth/module';
+import { SharedSecretAuthGuard } from './auth/sharedSecret.strategy';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     PrismaModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: SharedSecretAuthGuard,
+    },
+  ],
 })
 export class AppModule { }
--- src/env.ts
+++ src/env.ts
@@ -7,6 +7,13 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string(),
     DISCORD_WEBHOOK_URL: z.string(),
+    PRIMARY_API_KEY: z.string().optional(),
+    SECONDARY_API_KEY: z.string().regex(/^[^,\n]+,[^,\n]+(?:\n[^,\n]+,[^,\n]+)*$/).optional()
+      .transform(text => {
+        if (!text)
+          return;
+        return text.split('\n').map(entry => entry.replace(/,[^]*$/, ''));
+      }),
     PORT: z.coerce.number().default(3000),
     HOSTNAME: z.string().default('0.0.0.0'),
     // Sentry
